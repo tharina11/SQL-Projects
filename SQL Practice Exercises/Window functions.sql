4d6bb5070dbe3b62eda8e70f1e48c1f3618f53b9
Tharindu Abeysinghe <tha8761@gmail.com>
2026-03-25 00:49:33 -0500

    Complete window functions

--- SQL Practice Exercises/Window functions.sql
+++ SQL Practice Exercises/Window functions.sql
@@ -96,3 +96,32 @@ SELECT
 FROM journey
 JOIN train
 ON train.id = journey.train_id
+
+-- Multiple window functions
+SELECT 
+  journey.id,
+  train.production_year,
+  COUNT(journey.id) OVER(PARTITION BY train_id) AS journeys_by_train,
+  COUNT(journey.id) OVER(PARTITION BY route_id) AS journeys_by_route
+FROM journey
+JOIN train
+ON train.id = journey.train_id
+
+-- Window function with excluding one value
+SELECT 
+  ticket.id,
+  price,
+  date,
+  AVG(price) OVER(PARTITION BY date),
+  COUNT(ticket.id) OVER(PARTITION BY date)
+FROM ticket
+JOIN journey
+ON ticket.journey_id = journey.id
+WHERE train_id <> 5
+
+-- Division in a window function
+SELECT
+  id,
+  price,
+  price / SUM(price) OVER(PARTITION BY journey_id)::numeric AS ratio
+FROM ticket
