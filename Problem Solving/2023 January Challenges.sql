f1ef165cbff998374e9630e4ced2ab763e1b863f
Tharindu Abeysinghe <tha8761@gmail.com>
2026-05-09 22:44:44 -0400

    Add answers for two challenges

--- Problem Solving/2023 January Challenges.sql
+++ Problem Solving/2023 January Challenges.sql
@@ -23,3 +23,20 @@ SELECT
   SUM(total_price) - LAG(SUM(total_price)) OVER(ORDER BY payment_date) AS day_over_day_difference
 FROM booking
 GROUP BY payment_date;
+
+-- Challenge 4
+SELECT
+  payment_date,
+  SUM(total_price) AS day_sum,
+  LAG(SUM(total_price)) OVER(ORDER BY payment_date) AS previous_day_sum,
+  ROUND(SUM(total_price)*100 / LAG(SUM(total_price)) OVER(ORDER BY payment_date), 2) 
+    AS day_by_day_difference_percentage
+FROM booking
+GROUP BY payment_date;
+
+-- Challenge 5
+SELECT
+  payment_date,
+  total_price,
+  SUM(total_price) OVER(ORDER BY payment_date RANGE UNBOUNDED PRECEDING) AS running_total
+  FROM booking;
